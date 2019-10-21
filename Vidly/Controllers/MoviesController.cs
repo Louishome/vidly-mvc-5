d4535e4937aead183fe75c9ab9b244e4206a213f
Louis Yeh <louis.yeh@garmin.com>
2019-10-21 17:38:05 +0800

    Finished Exercise3
    Finished Exercise3
    
    Change-Id: Id84df8e09b0a313d89e074e7766dc9caf83ee7e1

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -7,6 +9,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext m_dbContext;
+
+        public MoviesController()
+        {
+            m_dbContext = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            m_dbContext.Dispose();
+        }
+
         public ViewResult Index()
         {
             var movies = GetMovies();
@@ -14,32 +29,17 @@ namespace Vidly.Controllers
             return View(movies);    
         }
 
-        private IEnumerable<Movie> GetMovies()
+        private IEnumerable<Movie> GetMovies() => m_dbContext.Movies.Include(m => m.GenreType).ToList();
+
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            if (movie == null)
             {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
+                return HttpNotFound();
+            }
 
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View(movie);
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,9 +1,24 @@
 ﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Movie
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int NumberInStock { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        [Required]
+        public Genre GenreType { get; set; }
     }
 }
